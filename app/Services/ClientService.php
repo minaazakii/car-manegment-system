ede782cf3f36868d26024d42924d2f445aa459ac
minaazaki <minaeragon@gmail.com>
2024-09-28 14:09:17 +0300

    refactor: make the logic of update client cars in client service

--- app/Services/ClientService.php
+++ app/Services/ClientService.php
@@ -8,6 +8,8 @@ use Illuminate\Pagination\LengthAwarePaginator;
 
 class ClientService
 {
+    public function __construct(private CarService $carService) {}
+
     public function getClients($paginated = true, $size = 10, $search = null): Collection | LengthAwarePaginator
     {
 
@@ -33,12 +35,44 @@ class ClientService
     public function updateClient($data, $id): Client
     {
         $client = Client::findOrFail($id);
-        $client->update($data);
+        $client->update([
+            'name' => $data['name'],
+            'phone' => $data['phone'],
+        ]);
         return $client;
     }
 
     public function deleteClient($id): void
     {
         Client::findOrFail($id)->delete();
+    }
+
+    public function updateClientCars(Client $client, array $cars): Client
+    {
+        $carsToDelete = $client->cars->pluck('id')->toArray();
+
+        foreach ($cars as $car) {
+
+            //Create car if it's not in the user Cars Ids
+            if (!isset($car['id'])) {
+                $this->carService->createCar($car, $client);
+                continue;
+            }
+
+            //Update car if it's in the user Cars Ids
+            $this->carService->updateCar($car, $car['id']);
+
+            //Remove Car from the list of cars to delete Since it's in the request
+            $carsToDelete = array_diff($carsToDelete, [$car['id']]);
+        }
+
+        //Delete cars that are not in the request
+        if ($carsToDelete) {
+            foreach ($carsToDelete as $carId) {
+                $this->carService->deleteCar($carId);
+            }
+        }
+
+        return $this->getSingleClient($client->id);
     }
 }
